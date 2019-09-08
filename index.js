02a1c82b97cccf4f2e6e85b551e57d39b09ed371
AliKazmierczak <ali.kazmierczak@gmail.com>
2019-09-08 23:41:42 +0100

    still need to correct what happens when hoover leaves room

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// let fileSystem = require("file-system");
 let fs = require('fs')
 
 let filename = "input.txt"
@@ -8,18 +7,51 @@ let roomDimentions = data[0].toString().split(" ").map(Number);
 let initHoover = data[1].toString().split(" ").map(Number);
 let directions = data[data.length -1].toString().split("");
 let dirt = data.slice(2,-1);
+let dirtCleared = 0;
+let route = [];
 
 dirt = dirt.map((element) => {
     return element.split(' ').map(Number)
-})
-//moveHoover(roomDimentions, initHoover, directions, dirt)
+});
 
-console.log('room dimentions are ', roomDimentions);
-console.log('initHoover = ', initHoover);
-console.log('dirt = ', dirt);
+moveHoover();
 
-// function moveHoover (room, hooverPosition, directions, target){
+console.log(route[route.length-1]);
+console.log(dirtCleared);
 
-// }
+function moveHoover (){
+    let x = initHoover[0]
+    let y = initHoover[1]
 
-
+    for (elem of directions){
+        if(elem === 'N'){
+            y++
+            hasBeenCleaned(x, y, dirt)
+            route.push([x, y])
+        }
+        if(elem === 'S'){
+            y--
+            hasBeenCleaned(x, y, dirt)
+            route.push([x, y])
+        }
+        if(elem === 'E'){
+            x++
+            hasBeenCleaned(x, y, dirt)
+            route.push([x, y])
+        }
+        if(elem === 'W'){
+            x--
+            hasBeenCleaned(x, y, dirt)
+            route.push([x, y])
+        }
+    }  
+}
+function hasBeenCleaned(x, y, dirtPatch) {
+    for (shotIndex in dirtPatch) {
+        let el = dirtPatch[shotIndex]
+        if (el[0] === x && el[1] === y) {
+            dirtPatch.splice(dirtPatch.indexOf([x, y]),1);
+            dirtCleared++
+        }
+    }
+};
