fde44c65b128b2f452fdeecc30df43fa35ccea42
AliKazmierczak <ali.kazmierczak@gmail.com>
2019-09-08 20:17:26 +0100

    got all numbers being read as Numbers not String

--- index.js
+++ index.js
@@ -4,21 +4,19 @@ let fs = require('fs')
 let filename = "input.txt"
 let data = fs.readFileSync(process.cwd() + "/" + filename).toString().split(/\r?\n/)
 
-let roomDimentions = data.slice(0,1).toString().split(" ").map(Number);
-let initHoover = data.slice(1,2).toString().split(" ").map(Number);
-let directions = data.slice(-1).toString().split("");
+let roomDimentions = data[0].toString().split(" ").map(Number);
+let initHoover = data[1].toString().split(" ").map(Number);
+let directions = data[data.length -1].toString().split("");
 let dirt = data.slice(2,-1);
 
-
+dirt = dirt.map((element) => {
+    return element.split(' ').map(Number)
+})
 //moveHoover(roomDimentions, initHoover, directions, dirt)
 
 console.log('room dimentions are ', roomDimentions);
-console.log(typeof(roomDimentions[0]))
-console.log('initHoover = ' + initHoover);
-console.log(typeof(initHoover[0]))
-// console.log('directions = ' + directions);
-console.log('dirt = ' + dirt);
-console.log(typeof(dirt[0]))
+console.log('initHoover = ', initHoover);
+console.log('dirt = ', dirt);
 
 // function moveHoover (room, hooverPosition, directions, target){
 
