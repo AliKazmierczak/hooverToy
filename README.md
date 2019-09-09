d63ff5e4da433f1ef8a77f8162a0fab176ce5367
AliKazmierczak <ali.kazmierczak@gmail.com>
2019-09-09 16:38:01 +0100

    Cleaning the code

--- README.md
+++ README.md
@@ -4,38 +4,52 @@
 This is a simple simulation of an imaginary robotic hoover navigating with given directions around an imaginary room with established dimentions.
 
 # Table of contents
+    * Requirements
     * Installation/Setup
+    * Running the code
     * Usage
     * Technologies
-    * Creator
-
+    * End notes
+# Requirements
+- Node.js >= 8.10.0
+- npm
 # Installation/Setup:
-- You need to install nodejs 10
-- then run
+- Run:
     ```
+    git clone git@github.com:AliKazmierczak/trayHoover.git
     npm install
     ```
-- for tests run
+# Running the code:
+- for tests run:
     ```
     npm test
     ```
-- for the application run
+- for the application run with 'input.txt' as data souce:
     ```
     npm start 
     ```
+- if you want to use other text file as source material (enter the target file and path in place of '.\newFileName.txt') run:
+    ```
+    node index.js .\newFileName.txt
+    ```
+- if you want to see a little twist I've added to the program run:
+    ```
+    npm fun-time
+    ```
+- or for custom file path:
+    ```
+    node play.js .\newFileName.txt
+    ```
 # Usage 
-
+This simulation has little usage outside of fun and games.
 ## This program
+Doesn't allow for invalid placement of the imaginary hoover inside the imaginary room.
 
 # Technolgies used:
+- node js
 - jest
 
-## Creator
-
-| Name                |
-| ------------------- |
-| **Alicja Kaźmierczak** |
-
 # End notes
-Although it wasn't required, I decided to add a few lines that check whether the hoover's initial position is whithin the established room. The simulation starts only if it is. 
-I did this to protect the code from working on invalid input.
+I really enjoyed this assignemt and thought of fun ways to toy with it. 
+I have more ideas how to improve this little simulation and I will most likely do it later on. I didn't do it now, since it essentially wasn't part of my assignment ;) 
+What I intend to do is to have the program provide a solution of additional directions, in case the imaginary room wasn't fully cleaned of dirt patches.
--- play.js
+++ play.js
@@ -4,16 +4,19 @@ let filename = process.argv[2]
 let data = fs.readFileSync(process.cwd() + "/" + filename).toString().split(/\r?\n/)
 
 let roomDimentions = data[0].toString().split(" ").map(Number);
-let initHoover = data[1].toString().split(" ").map(Number);
+let hooverPosition = data[1].toString().split(" ").map(Number);
 let directions = data[data.length - 1].toString().split("");
 let dirt = data.slice(2, -1);
+let initialDirt = dirt
 let dirtCleared = 0;
 
 dirt = dirt.map((element) => {
     return element.split(' ').map(Number)
 });
 
-isHooverInRoom(roomDimentions, initHoover);
+isHooverInRoom(roomDimentions, hooverPosition);
+
+letHaveSomeFun();
 
 function moveHoover(hoover, direction, room) {
     let x = hoover[0]
@@ -45,8 +48,8 @@ function moveHoover(hoover, direction, room) {
     console.log(dirtCleared);
 };
 function hasBeenCleaned(x, y, dirtPatch) {
-    for (shotIndex in dirtPatch) {
-        let el = dirtPatch[shotIndex]
+    for (dirtIndex in dirtPatch) {
+        let el = dirtPatch[dirtIndex]
         if (el[0] === x && el[1] === y) {
             dirtPatch.splice(dirtPatch.indexOf([x, y]), 1);
             dirtCleared++
@@ -58,5 +61,24 @@ function isHooverInRoom(room, hoover) {
         return console.error(400, 'Your imaginary hoover was never placed in the room! Simulation aborted :(');
     }
     else
-        moveHoover(initHoover, directions, roomDimentions);
-}
+        moveHoover(hooverPosition, directions, roomDimentions);
+};
+function letHaveSomeFun() {
+    if (dirt.length === 0) {
+        console.log('\x1b[33m', "CONGRATULATIONS!", '\n',
+            '\x1b[32m', 'The room is clean now! :)', '\x1b[0m')
+    }
+    else
+        if (dirtCleared != 0) {
+            console.log('\x1b[35m',
+                'I did the instructions but the room is still dirty :(', '\n',
+                '\x1b[32m',
+                'The room is however', (dirtCleared / initialDirt.length) * 100,
+                '% cleaner, so good job, I guess xD', '\x1b[0m')
+        }
+        else
+            console.log('\x1b[35m',
+                'I did the instructions but the room is still dirty :(', '\n',
+                "\x1b[31m",
+                "You didn't manage to clean ANYTHING! I'm dissapointed in you :P", '\x1b[0m')
+};
